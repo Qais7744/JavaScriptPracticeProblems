dcabd161a9460dbb89733f42d7b2da54864be542
unknown <altamashbagwan1@gmail.com>
2021-11-25 00:54:27 +0530

    Added: Functioning of Sequence Practice Problem

---
+++ RandomProblems.groovy
@@ -0,0 +1,47 @@
+//Use REPL – Random Function Math.floor(Math.random() * 10); to get SingleDigit.
+
+> Math.floor(Math.random() * 10);
+1
+> Math.floor(Math.random() * 10);
+7
+> Math.floor(Math.random() * 10);
+9
+> Math.floor(Math.random() * 10);
+9
+> Math.floor(Math.random() * 10);
+2
+> Math.floor(Math.random() * 10);
+3
+
+//Use REPL – Use Random to get Dice Number between 1 to 6
+
+> 1 + Math.floor(Math.random() * 10) % 6;
+5
+> 1 + Math.floor(Math.random() * 10) % 6;
+4
+> 1 + Math.floor(Math.random() * 10) % 6;
+1
+> 1 + Math.floor(Math.random() * 10) % 6;
+5
+> 1 + Math.floor(Math.random() * 10) % 6;
+4
+> 1 + Math.floor(Math.random() * 10) % 6;
+3
+
+//Use REPL – Add two Random Dice Number and Print the Result
+
+> (1 + Math.floor(Math.random() * 10) % 6) + (1 + Math.floor(Math.random() * 10)
+...  % 6)
+3
+> (1 + Math.floor(Math.random() * 10) % 6) + (1 + Math.floor(Math.random() * 10)
+...  % 6)
+11
+> (1 + Math.floor(Math.random() * 10) % 6) + (1 + Math.floor(Math.random() * 10)
+...  % 6)
+7
+> (1 + Math.floor(Math.random() * 10) % 6) + (1 + Math.floor(Math.random() * 10)
+...  % 6)
+4
+> (1 + Math.floor(Math.random() * 10) % 6) + (1 + Math.floor(Math.random() * 10)
+...  % 6)
+5
